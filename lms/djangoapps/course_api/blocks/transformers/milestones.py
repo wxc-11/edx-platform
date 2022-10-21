ec42f9c5d5f601e108ebbcade258b03b8089c856
varshamenon4 <varshamenon04@gmail.com>
2022-10-21 12:16:45 +0000

    style: remove trailing whitespace

--- lms/djangoapps/course_api/blocks/transformers/milestones.py
+++ lms/djangoapps/course_api/blocks/transformers/milestones.py
@@ -172,7 +172,7 @@ class MilestonesAndSpecialExamsTransformer(BlockStructureTransformer):
 
         return False
 
-    def _generate_special_exam_attempt_context(self, is_practice_exam, is_proctored_enabled, 
+    def _generate_special_exam_attempt_context(self, is_practice_exam, is_proctored_enabled,
             is_timed_exam, user_id, course_key, block_key):
         """
         Helper method which generates the special exam attempt context.
